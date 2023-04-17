8a6e4320b2ea6bb73a82296494351216220ec9df
OmetovVlad <ometov96@mail.ru>
2023-04-17 18:53:34 +0700

    first commit

---
+++ src/js/modules/functions.js
@@ -0,0 +1,60 @@
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+import events from "events";
+
+export function isWebp() {
+	// Проверка поддержки webp
+	function testWebP(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+	// Добавление класса _webp или _no-webp для HTML
+	testWebP(function (support) {
+		let className = support === true ? 'webp' : 'no-webp';
+		document.documentElement.classList.add(className);
+	});
+}
+
+/* Поиск города на главной */
+export function subMenu(){
+	const subMenuToggle = document.querySelectorAll('.header .toggle_item');
+
+	if (subMenuToggle) {
+		subMenuToggle.forEach(function (item) {
+			item.addEventListener("click", toggleSubMenu);
+			return true;
+		});
+
+		function toggleSubMenu () {
+			if (this.classList.contains("open")) {
+				this.classList.remove("open");
+			} else {
+				this.classList.add("open");
+			}
+		}
+	}
+
+}
+
+/* Поиск города на главной */
+export function openMenu(){
+	const openMenuButton = document.querySelector('.header .openMenu');
+	const menuElement = document.querySelector('.main-menu');
+
+	if (openMenuButton) {
+		openMenuButton.addEventListener("click", toggleMenu);
+
+		function toggleMenu () {
+			if (menuElement.classList.contains("open")) {
+				this.classList.remove("open");
+				menuElement.classList.remove("open");
+			} else {
+				this.classList.add("open");
+				menuElement.classList.add("open");
+			}
+		}
+	}
+
+}
